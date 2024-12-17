e4f9f0e192215fe5d4b0712e8e067f873af2b545
JustMargata <66246000+JustMargata@users.noreply.github.com>
2024-12-17 11:13:06 +0200

    .

---
+++ Domashni/Klasove/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasove
+{
+    public class Computer
+    {
+        private string procesor;
+        private string videoKarta;
+        private int ram;
+        private int hdd;
+
+        public string Procesor
+        {
+            get { return procesor; }
+            set { procesor = value; }
+        }
+        public string VideoKarta
+        {
+            get { return videoKarta; }
+            set { videoKarta = value; }
+        }
+        public int Ram
+        {
+            get { return ram; }
+            set { ram = value; }
+        }
+        public int Hdd
+        {
+            get { return hdd; }
+            set { hdd = value; }
+        }
+        public Computer(string procesor, string videoKarta, int ram, int hdd)
+        {
+            this.procesor = procesor;
+            this.videoKarta = videoKarta;
+            this.ram = ram;
+            this.hdd = hdd;
+        }
+        public void Izhod()
+        {
+            Console.WriteLine($"CPU: {procesor} | GPU: {videoKarta} | Ram: {ram} GB| HDD: {hdd} GB");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Computer computer1 = new Computer("Intel Core i5", "Nvdia Geforce 1050ti", 16, 1000);
+            computer1.Izhod();
+
+        }
+
+    }
+}
---
+++ Domashni/Triugulnik/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triugulnik
+{
+    public class Triugulnik
+    {
+        private double stra;
+        private double strb;
+        private double strc;
+        private double liceFormula;
+        private double perimeterFormula;
+        public double Stra
+        {
+            get { return stra;}
+            set { stra = value; }
+        }
+        public double Strb
+        {
+            get { return strb; }
+            set { strb = value; }
+        }
+        public double Strc
+        {
+            get { return strc; }
+            set { strc = value; }
+        }
+        public double LiceFormula 
+        {
+            get { return liceFormula; }
+            set { liceFormula = value; }
+        }
+        public double PerimeterFormula
+        {
+            get { return perimeterFormula; }
+            set { perimeterFormula = value; }
+        }
+        public Triugulnik(double stra, double strb, double strc)
+        {
+            this.stra = stra;
+            this.strb = strb;
+            this.strc = strc;
+        }
+        
+        public void Lice()
+        {
+            liceFormula = (stra * strb) / 2;
+        }
+        public void Perimeter()
+        {
+            perimeterFormula = stra + strb + strc;
+        }
+        public void Izhod()
+        {
+            Lice();
+            Perimeter();
+            Console.WriteLine($"Страна A: {stra} | Страна B: {strb} | Страна C: {strc} | Лице: {liceFormula} | Периметър: {perimeterFormula} ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи страна a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Въведи страна b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Въведи страна c: ");
+            double c = double.Parse(Console.ReadLine());
+            Triugulnik triugulnik1 = new Triugulnik(a, b, c);
+            Triugulnik triugulnik2 = new Triugulnik(2, 7, 9);
+            List<Triugulnik> listtriug = new List<Triugulnik>() { triugulnik1, triugulnik2 };
+            int i = 1;
+            foreach (Triugulnik p in listtriug)
+            {
+                Console.WriteLine($"Триъгълник {i}: ");
+                p.Izhod();
+                i++;
+            }
+        }
+    }
+}
